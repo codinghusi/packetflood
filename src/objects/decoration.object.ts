6eb45001c746b92e81491479590d9069e04f192a
Gerrit Weiermann <gerrit.weiermann@gmx.de>
2021-01-20 15:20:42 +0100

    removed debugging text

--- src/objects/decoration.object.ts
+++ src/objects/decoration.object.ts
@@ -6,31 +6,4 @@ import * as PIXI from 'pixi.js';
 
 @game.forGroup('decoration')
 class DecorationObject extends GameObject {
-    @Event.Init
-    init() {
-        if (this.id === 'wall') {
-            const text = new PIXI.Text(`${this.world.x}, ${this.world.y}, ${this.zIndex}`, {
-                fontSize: 30,
-                fill: 0xeeeeee,
-                decoration: 'underline',
-            });
-            text.position.x = this.x + 80;
-            text.position.y = this.y + 25;
-            text.zIndex = 10000;
-    
-            this.engine.app.stage.addChild(text);
-        }
-
-        if (this.id === 'grassblock' && false) {
-            const text = new PIXI.Text(`${this.world.x}, ${this.world.y}`, {
-                fontSize: 30,
-                fill: 0xffffff
-            });
-            text.position.x = this.x + 80;
-            text.position.y = this.y + 55;
-            text.zIndex = 10000;
-    
-            this.engine.app.stage.addChild(text);
-        }
-    }
 }
